ef37ba60b520473a5ab1c8289a69e2d0030852e3
Hyumeun <rafael-1440973@educar.rs.gov.br>
2023-03-03 21:38:17 -0300

    Aula 3 iniciada

---
+++ Aula03/Exercicio1/script.js
@@ -0,0 +1,11 @@
+//Construa uma função que retorne um array contendo todos os numeros pares encontrados entre 10 e 50.
+
+const paresVector = () => {
+    const arrayUm = []
+    for(let i=10; i<=50; i++) {
+        if(i % 2 === 0) {
+            arrayUm.push(i)
+        }
+    }
+    return arrayUm
+}
