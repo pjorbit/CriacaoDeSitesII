ed261ee9436baf6549fc4ff7e57ebd0d6f9260e3
Hyumeun <rafael-1440973@educar.rs.gov.br>
2023-03-10 20:41:32 -0300

    Aula 3 iniciada

--- Aula03/Exercicio3/script.js
+++ Aula03/Exercicio3/script.js
@@ -1,18 +1,1 @@
-//Contrua uma função que gere um array de numeros inteiros aleatorios. 
-//É necessário informar a função a quantidade de numeros inteiros que o array deve conter.
-
-let a = prompt('Entre com o tamanho do array: ');
-a = Number(a);
-
-const arrayTamanho = () => {
-    const array = [];
-    for(let i=0; i<=a; i++) {
-        let aleatorio = Math.floor(Math.random() * 10)
-        array.push(aleatorio)
-    }
-    return array
-}
-
-console.log(arrayTamanho());
-
-//CONCLUIDO
+//Construa uma função que receba um array de numeros inteiros e retorne um array contendo os fatoriais destes números.
